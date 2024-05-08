85fa6c596355eb9a606617df125126f20a00a3d7
pranav69-icefire <115442130+pranav69-icefire@users.noreply.github.com>
2024-05-08 23:55:55 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,81 @@
+import Store from "./store.js";
+import View from "./view.js";
+
+// window.addEventListener("load", App.init);
+
+const players = [
+  {
+    id: 1,
+    name: "player 1",
+    iconClass: "fa-x",
+    colorClass: "turquoise",
+  },
+  {
+    id: 2,
+    name: "player 2",
+    iconClass: "fa-o",
+    colorClass: "yellow",
+  },
+];
+
+function init() {
+  const view = new View();
+  const store = new Store("live-t3-storage-key", players);
+
+  function initView() {
+    view.closeAll();
+    view.clearMoves();
+    view.setTurnIndicator(store.game.currentPlayer);
+    view.updateScoreboard(
+      store.stats.playerWithStats[0].wins,
+      store.stats.playerWithStats[1].wins,
+      store.stats.ties
+    );
+    view.initializeMoves(store.game.moves);
+  }
+  window.addEventListener("storage", () => {
+    console.log("state changed from another tab");
+    initView();
+  });
+
+  initView();
+
+  view.bindGameResetEvent((event) => {
+    store.reset();
+
+    initView();
+  });
+
+  view.bindNewRoundEvent((event) => {
+    store.newRound();
+
+    initView();
+  });
+
+  view.bindPlayerMoveEvent((square) => {
+    const existingMove = store.game.moves.find(
+      (move) => move.squareId === +square.id
+    );
+    if (existingMove) {
+      return;
+    }
+    // Place an icon of current player in a square
+    view.handlePlayerMove(square, store.game.currentPlayer);
+
+    // Advance to the next state by pushing a move to the moves array
+    store.playerMove(+square.id);
+
+    if (store.game.status.isComplete) {
+      view.openModal(
+        store.game.status.winner
+          ? `${store.game.status.winner.name} wins!`
+          : "Tie!"
+      );
+      return;
+    }
+
+    // Set the next player's turn indicator
+    view.setTurnIndicator(store.game.currentPlayer);
+  });
+}
+window.addEventListener("load", init);
